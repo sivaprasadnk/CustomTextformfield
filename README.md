c6849e02ed135484db6a00a04c57c546b851fea1
sivaprasadnk <sivaprasadnk123@gmail.com>
2022-12-06 09:14:24 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,45 @@
+# Text2Pdf
+Custom textformfield with options to add leading and trailing text, appended at begining and end of input
+
+![Image](https://github.com/sivaprasadnk/Text2Pdf/blob/master/screenshots/screenshot1.jpeg)
+![Image](https://github.com/sivaprasadnk/Text2Pdf/blob/master/screenshots/screenshot2.jpeg)
+
+
+## Getting Started
+
+Add this to your package's `pubspec.yaml` file
+
+```yaml
+
+dependencies:
+  custom_textformfield: ^0.0.1
+```
+
+## Usage
+
+First, you just have to import the package using:
+
+```dart
+import 'package:custom_textformfield/custom_textformfield.dart';
+```
+
+Then , use the widget as below, 
+
+
+```dart
+
+  @override
+  Widget build(BuildContext context) {
+    return CustomTextFormField(
+      padding: const EdgeInsets.all(10),
+      onChanged: (val) {},
+      leadingText: 'https://',
+      suffixIcon: const Icon(
+        Icons.check,
+        color: Colors.green,
+      ),
+      trailingText: ".com",
+    );
+```
+## Contributing
+Pull requests are welcome. For major changes, please open an issue first to discuss what you would like to change.
---
+++ example/lib/main.dart
@@ -0,0 +1,51 @@
+import 'package:custom_textformfield/custom_textformfield.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Custom TextFormField Example',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Custom TextFormField Example'),
+      ),
+      body: Column(
+        children: [
+          const SizedBox(height: 70),
+          CustomTextFormField(
+            padding: const EdgeInsets.all(10),
+            onChanged: (val) {},
+            leadingText: 'https://',
+            trailingText: ".com",
+          )
+        ],
+      ),
+    );
+  }
+}
